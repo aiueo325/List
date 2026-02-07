fbe2b650ea312576ba90bc5786738fbfaea1a888
aiueo <uaoi@icloud.com>
2026-02-08 08:33:37 +0900

    Initial Commit

---
+++ List/DetailView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+import AVFAudio
+
+struct DetailView: View {
+    
+    @State var animal: Animal
+    @State var player: AVAudioPlayer!
+    
+    var body: some View {
+        VStack {
+            Text(animal.name)
+                .font(.title)
+                .bold()
+            Spacer()
+            Image(animal.imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .shadow(radius: 10)
+            Spacer()
+            Button {
+            }label: {
+                Text("鳴き声を聞く")
+            }
+            .buttonStyle(BorderedProminentButtonStyle())
+            Spacer()
+            Spacer()
+        }
+        .padding()
+        .onAppear{
+            player = try! AVAudioPlayer(data: animal.sound)
+        }
+    }
+}
+
+#Preview {
+    DetailView(animal: .cat)
+}
---
+++ List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by Aoi Ueno on 2026/02/08.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
